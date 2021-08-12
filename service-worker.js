5338b1c46ceea92b1ed76cb65acdd683f7ac8863
Artem Fedoseev <afedoseev359@gmail.com>
2021-08-12 15:57:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.ccde77bd203eda6dec5a540de7403245.js"
 );
 
 self.addEventListener('message', (event) => {
